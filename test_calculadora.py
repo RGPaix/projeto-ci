19169daef79ca68165c638beb3c572c54f6a4fb6
Rodrigo <rodleidenschaft@gmail.com>
2025-09-04 21:27:50 -0300

    feat: Commit inicial do projeto e testes

---
+++ test_calculadora.py
@@ -0,0 +1,14 @@
+import unittest
+from calculadora import somar
+
+
+class TestCalculadora(unittest.TestCase):
+    """
+    Suíte de testes para o módulo da calculadora.
+    """
+
+    def test_soma_positivos(self):
+        """
+        Verifica se a função 'somar' opera corretamente com números positivos.
+        """
+        self.assertEqual(somar(5, 3), 8)
